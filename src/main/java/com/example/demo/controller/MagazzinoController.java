685ad185d0e47160afc6ffe2678a895211ec5201
Giovanni Capuano <gc30109720@gmail.com>
2026-01-15 17:42:02 +0100

    last update

--- src/main/java/com/example/demo/controller/MagazzinoController.java
+++ src/main/java/com/example/demo/controller/MagazzinoController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Comparator;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/prodotti")
